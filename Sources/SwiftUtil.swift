2c3f45503811d6285e585c74b1819fe36723b353
Josh Donnoe <josh.donnoe@gmail.com>
2017-05-21 15:10:13 -0500

    public func no class

--- Sources/SwiftUtil.swift
+++ Sources/SwiftUtil.swift
@@ -1,11 +1,9 @@
 import BigInt
 import Darwin
-public class SwiftUtil {
-    public static func readInt(_ s : String) -> BigInt {
-        print(s, terminator: "")
-        return BigInt(Int(readLine()!)!)
-    }
+public func readInt(_ s : String) -> BigInt {
+    print(s, terminator: "")
+    return BigInt(Int(readLine()!)!)
+}
 //    func <(left: BigInt, right: Int) -> Bool {
 //        return left < BigInt(right)
 //    }
-}
