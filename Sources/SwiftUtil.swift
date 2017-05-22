7ec78646da783194f5bef0e66da7c3c56455e578
Josh Donnoe <josh.donnoe@gmail.com>
2017-05-21 20:04:25 -0500

    overload big < int

--- Sources/SwiftUtil.swift
+++ Sources/SwiftUtil.swift
@@ -4,6 +4,6 @@ public func readInt(_ s : String) -> BigInt {
     print(s, terminator: "")
     return BigInt(Int(readLine()!)!)
 }
-//    func <(left: BigInt, right: Int) -> Bool {
-//        return left < BigInt(right)
-//    }
+public func <(left: BigInt, right: Int) -> Bool {
+    return left < BigInt(right)
+}
