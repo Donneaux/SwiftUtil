e1d91c22437bf7453893cabc2993044334e9976f
Josh Donnoe <josh.donnoe@gmail.com>
2017-05-22 09:46:09 -0500

    Update SwiftUtil.swift

--- Sources/SwiftUtil.swift
+++ Sources/SwiftUtil.swift
@@ -4,6 +4,9 @@ public func readInt(_ s : String) -> BigInt {
     print(s, terminator: "")
     return BigInt(Int(readLine()!)!)
 }
-//    func <(left: BigInt, right: Int) -> Bool {
-//        return left < BigInt(right)
-//    }
+public func <(left: BigInt, right: Int) -> Bool {
+    return left < BigInt(right)
+}
+public func <=(left: BigInt, right: Int) -> Bool {
+    return left <= BigInt(right)
+}
